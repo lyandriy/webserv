efad182c091203d5b683cd0e0cd806fe6e6142b4
PepeGF <gran.pepe@gmail.com>
2024-10-02 15:57:12 +0200

    WIP - Reajustando recepción de partes de requests

--- src/Request.cpp
+++ src/Request.cpp
@@ -6,7 +6,7 @@ Request::Request(int free_pfd, int new_sock) : _fd_socket(free_pfd), _pos_socket
 											_method(""), _protocol(""), _host(""), _port(0),
 											_body(), _help_message(), _valid(true), _error_code(0), _headers(),
 											_params(), _request(), _accept_method(), _request_line(""), _lines(),
-											_type(0), _status(0)
+											_type(0), _status(1)
 {
 	debug = true;
 }
