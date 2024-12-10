31e252de57b02f1e3a5ed6cee71cc007798cf1db
PepeGF <gran.pepe@gmail.com>
2024-12-10 22:54:14 +0100

    más tests. Pendiente tests redirecciones

--- test/testing_script.py
+++ test/testing_script.py
@@ -1,196 +1,391 @@
 import requests
 from colorama import Fore, Style
+import os
 
 def another_request():
 	x = input("Enviar siguiente petición?:\n")
 	if not (x == 'y' or x == 'yes' or x == 's' or x == 'si' or len(x) == 0):
 		exit()
 
-press_to_continue = "Presiona una tecla para continuar.\n"
-# 0 pruebas para la configuración correcta completa: (config/conf)
-input("Esta es una request sencilla a la página principal por defecto \
-	  del servidor\n" + press_to_continue)
+# press_to_continue = "Presiona una tecla para continuar.\n"
+# # 0 pruebas para la configuración correcta completa: (config/conf)
+# input("Esta es una request sencilla a la página principal por defecto \
+# 	  del servidor\n" + press_to_continue)
 
 url_base = 'http://localhost:8081'
 headers = {
     "Connection": "keep-alive",
+    # "Connection": "close",
 }
 test = 0
 
+def test1():
 # 1 petición a ruta por defecto (index)
-test += 1
-url = url_base + ''
-response = requests.get(url, headers=headers)
-print("URL de la petición: ", url)
-print("Código de respuesta:\n", response.status_code)
-print("Cabeceras de la respuesta:\n", response.headers)
-file = open("html/files/index.html", 'r')
-content = file.read()
-file.close()
-if response.text == content:
-	print(Fore.GREEN + f"TEST {test} CORRECTO")
-else:
-	print(Fore.RED + f"TEST {test} INCORRECTO")
-print(Style.RESET_ALL)
-with open("test/outputs/primera_basica.html", 'w+') as f:
-	f.write(response.text)
-print("\n" + "-"*50 + "\n")
+	global test
+	test += 1
+	url = url_base + ''
+	response = requests.get(url, headers=headers)
+	print("URL de la petición: ", url)
+	print("Código de respuesta:\n", response.status_code)
+	print("Cabeceras de la respuesta:\n", response.headers)
+	file = open("html/files/index.html", 'r')
+	content = file.read()
+	file.close()
+	if response.text == content:
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	print(Style.RESET_ALL)
+	print("\n" + "-"*50 + "\n")
 
-# another_request()
+	# another_request()
 
-
+def test2():
 # 2 peticion sencilla de archivo normal
-test += 1
-url = url_base + '/prueba.html'
-response = requests.get(url, headers=headers)
-print("URL de la petición: ", url)
-print("Código de respuesta:\n", response.status_code)
-print("Cabeceras de la respuesta:\n", response.headers)
-file = open("html/files/prueba.html", 'r')
-content = file.read()
-file.close()
-if response.text == content:
-	print(Fore.GREEN + f"TEST {test} CORRECTO")
-else:
-	print(Fore.RED + f"TEST {test} INCORRECTO")
-print(Style.RESET_ALL)
-print("\n" + "-"*50 + "\n")
+	global test
+	test += 1
+	url = url_base + '/prueba.html'
+	response = requests.get(url, headers=headers)
+	print("URL de la petición: ", url)
+	print("Código de respuesta:\n", response.status_code)
+	print("Cabeceras de la respuesta:\n", response.headers)
+	file = open("html/files/prueba.html", 'r')
+	content = file.read()
+	file.close()
+	if response.text == content:
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	print(Style.RESET_ALL)
+	print("\n" + "-"*50 + "\n")
 
-# another_request()
+	# another_request()
 
+def test3():
+	# 3 respuesta muy grande
+	global test
+	test += 1
+	url = url_base + "/el_quijote.txt"
+	response = requests.get(url, headers=headers)
+	print("URL de la petición: ", url)
+	print("Código de respuesta:\n", response.status_code)
+	print("Cabeceras de la respuesta:\n", response.headers)
+	file = open("html/files/el_quijote.txt", 'r', encoding='utf-8')
+	content = file.read()
+	file.close()
+	if response.text[:1000] == content[:1000]:
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	print(Style.RESET_ALL)
 
-# 3 respuesta muy grande
-test += 1
-url = url_base + "/el_quijote.txt"
-response = requests.get(url, headers=headers)
-print("URL de la petición: ", url)
-print("Código de respuesta:\n", response.status_code)
-print("Cabeceras de la respuesta:\n", response.headers)
-file = open("html/files/el_quijote.txt", 'r', encoding='utf-8')
-content = file.read()
-file.close()
-if response.text[:1000] == content[:1000]:
-	print(Fore.GREEN + f"TEST {test} CORRECTO")
-else:
-	print(Fore.RED + f"TEST {test} INCORRECTO")
-print(Style.RESET_ALL)
+	# another_request()
 
-# another_request()
+def test4():
+	# 4 petición errónea
+	global test
+	test += 1
+	url = url_base + "/esto_no_existe.html"
+	response = requests.get(url, headers=headers)
+	print("URL de la petición: ", url)
+	print("Texto de la respuesta:\n", response.text)
+	print("Código de respuesta:\n", response.status_code)
+	print("Cabeceras de la respuesta:\n", response.headers)
+	# esta ruta viene del archivo de configuración
+	# 	error_page 404 html/files/index.html;
+	file = open("html/files/index.html", 'r', encoding='utf-8') 
+	content = file.read()
+	file.close()
+	if response.text == content:
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	print(Style.RESET_ALL)
 
+	# another_request()
 
-# 4 petición errónea
-test += 1
-url = url_base + "/esto_no_existe.html"
-response = requests.get(url, headers=headers)
-print("URL de la petición: ", url)
-print("Texto de la respuesta:\n", response.text)
-print("Código de respuesta:\n", response.status_code)
-print("Cabeceras de la respuesta:\n", response.headers)
-# esta ruta viene del archivo de configuración
-# 	error_page 404 html/files/index.html;
-file = open("html/files/index.html", 'r', encoding='utf-8') 
-content = file.read()
-file.close()
-if response.text == content:
-	print(Fore.GREEN + f"TEST {test} CORRECTO")
-else:
-	print(Fore.RED + f"TEST {test} INCORRECTO")
-print(Style.RESET_ALL)
+def test5():
+	# 5 autoindex correcto
+	global test
+	test += 1
+	url = url_base + "/python"
+	response = requests.get(url, headers=headers)
+	print("URL de la petición: ", url)
+	# print("Texto de la respuesta:\n", response.text)
+	print("Código de respuesta:\n", response.status_code)
+	print("Cabeceras de la respuesta:\n", response.headers)
+	# si exista esta etiqueta de html es que se ha generado una lista
+	if response.text.find("<li><a href=") != -1: 
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	print(Style.RESET_ALL)
 
-# another_request()
+	# another_request()
 
-
-# 5 autoindex correcto
-test += 1
-url = url_base + "/python"
-response = requests.get(url, headers=headers)
-print("URL de la petición: ", url)
-# print("Texto de la respuesta:\n", response.text)
-print("Código de respuesta:\n", response.status_code)
-print("Cabeceras de la respuesta:\n", response.headers)
-# si exista esta etiqueta de html es que se ha generado una lista
-if response.text.find("<li><a href=") != -1: 
-	print(Fore.GREEN + f"TEST {test} CORRECTO")
-else:
-	print(Fore.RED + f"TEST {test} INCORRECTO")
-print(Style.RESET_ALL)
-
-# another_request()
-
-# 6 autoindex incorrecto
-test += 1
-url = url_base + "/directorio_inexistente"
-print("URL de la petición: ", url)
-response = requests.get(url, headers=headers)
-print("Texto de la respuesta:\n", response.text)
-print("Código de respuesta:\n", response.status_code)
-print("Cabeceras de la respuesta:\n", response.headers)
-file = open("serverHTML/error/404.html", 'r')
-content = file.read()
-file.close()
-if response.text == content:
-	print(Fore.GREEN + f"TEST {test} CORRECTO")
-else:
-	print(Fore.RED + f"TEST {test} INCORRECTO")
-print(Style.RESET_ALL)
-
-# another_request()
-
-
-# 7 servername incorrecto
-test += 1
-url = 'http://wololo:8081'
-try:
+def test6():
+	# 6 autoindex incorrecto
+	global test
+	test += 1
+	url = url_base + "/directorio_inexistente"
 	print("URL de la petición: ", url)
 	response = requests.get(url, headers=headers)
 	print("Texto de la respuesta:\n", response.text)
 	print("Código de respuesta:\n", response.status_code)
 	print("Cabeceras de la respuesta:\n", response.headers)
-except requests.exceptions.ConnectionError:
-	print(Fore.GREEN + f"TEST {test} CORRECTO")
-else:
-	print(Fore.RED + f"TEST {test} INCORRECTO")
-finally:
+	file = open("serverHTML/error/404.html", 'r')
+	content = file.read()
+	file.close()
+	if response.text == content:
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
 	print(Style.RESET_ALL)
 
-# another_request()
+	# another_request()
 
-# 8 servername incorrecto en header
-test += 1
-url = url_base + "/"
-headers['Host'] = 'wololo:8081'
-print(headers)
-print("URL de la petición: ", url)
-response = requests.get(url, headers=headers)
-print("Texto de la respuesta:\n", response.text)
-print("Código de respuesta:\n", response.status_code)
-print("Cabeceras de la respuesta:\n", response.headers)
-file = open("serverHTML/error/400.html", 'r')
-content = file.read()
-file.close()
-if response.text == content:
-	print(Fore.GREEN + f"TEST {test} CORRECTO")
-else:
-	print(Fore.RED + f"TEST {test} INCORRECTO")
-print(Style.RESET_ALL)
+def test7():
+	# 7 servername incorrecto
+	global test
+	test += 1
+	url = 'http://wololo:8081'
+	try:
+		print("URL de la petición: ", url)
+		response = requests.get(url, headers=headers)
+		print("Texto de la respuesta:\n", response.text)
+		print("Código de respuesta:\n", response.status_code)
+		print("Cabeceras de la respuesta:\n", response.headers)
+	except requests.exceptions.ConnectionError:
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	finally:
+		print(Style.RESET_ALL)
+
+	# another_request()
+
+def test8():
+	# 8 servername incorrecto en header
+	global test
+	test += 1
+	url = url_base + "/"
+	headers['Host'] = 'wololo:8081'
+	print(headers)
+	print("URL de la petición: ", url)
+	response = requests.get(url, headers=headers)
+	print("Texto de la respuesta:\n", response.text)
+	print("Código de respuesta:\n", response.status_code)
+	print("Cabeceras de la respuesta:\n", response.headers)
+	file = open("serverHTML/error/400.html", 'r')
+	content = file.read()
+	file.close()
+	if response.text == content:
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	print(Style.RESET_ALL)
+	another_request()
+
+def test9():
+	# 9 usando location con redirección
+	global test
+	test += 1
+	url = url_base + "/hola"
+	headers['Host'] = 'localhost:8081'
+	response = requests.get(url, headers=headers)
+	print("Texto de la respuesta:\n", response.text)
+	print("Código de respuesta:\n", response.status_code)
+	print("Cabeceras de la respuesta:\n", response.headers)
+	file = open("html/test/redirect/prueba.html", 'r')
+	content = file.read()
+	file.close()
+	if response.text == content: # and response.status_code in (range(300, 400))# añadir comprobación de response.status_code
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	print(Style.RESET_ALL)
+	another_request()
+
+def test10():
+	# 10 petición GET con body
+	global test
+	test += 1
+	url = url_base + "/"
+	headers['Host'] = 'localhost:8081'
+	headers['Content-Type'] = 'text/plain'
+	body_content = "Este es un pqueño body para el test"
+	response = requests.get(url, headers=headers, data=body_content)
+	file = open("html/files/index.html", 'r')
+	content = file.read()
+	file.close()
+	if response.text == content:
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	print(Style.RESET_ALL)
+	print("\n" + "-"*50 + "\n")
+
+	# another_request()
+
+def test11():
+	# 11 petición POST básica crea archivo wololo.txt
+	try:
+		os.remove("html/files/wololo.txt")
+	except FileNotFoundError:
+		pass
+	global test
+	test += 1
+	url = url_base + "/wololo.txt"
+	headers['Host'] = 'localhost:8081'
+	headers['Content-Type'] = 'text/plain'
+	body_content = "Este es un simple body para el test\n"
+	response = requests.post(url, headers=headers, data=body_content)
+	file = open("serverHTML/postResponse.html", 'r')
+	content = file.read()
+	file.close()
+	try:
+		file2 = open("html/files/wololo.txt", 'r')
+		content2 = file2.read()
+		file2.close()
+	except FileExistsError:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+		return	
+	if response.text == content and body_content == content2:
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	print(Style.RESET_ALL)
+	print("\n" + "-"*50 + "\n")
+
+	input("""El archivo wololo.txt ha sido creado, si no, 
+	   \rel siguiente test se rompe, puedes comprobarlo ahora 
+	   \ro pulsar cualquier tecla para continuar""")
+	test12()
+
+def test12():
+	# 12 petición POST básica añade informacion al archivo wololo.txt
+	global test
+	test += 1
+	try:
+		file2 = open("html/files/wololo.txt", 'r')
+		wololo_antes = file2.read()
+		file2.close()
+	except FileExistsError:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+		return
+	url = url_base + "/wololo.txt"
+	headers['Host'] = 'localhost:8081'
+	headers['Content-Type'] = 'text/plain'
+	body_content = "Este es un otro body para el test 12\n"
+	response = requests.post(url, headers=headers, data=body_content)
+	file = open("serverHTML/postResponse.html", 'r')
+	content = file.read()
+	file.close()
+	try:
+		file2 = open("html/files/wololo.txt", 'r')
+		wololo_despues = file2.read()
+		file2.close()
+		os.remove("html/files/wololo.txt")
+	except FileExistsError:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+		return
+	if response.text == content and wololo_antes + body_content == wololo_despues:
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	print(Style.RESET_ALL)
+	print("\n" + "-"*50 + "\n")
+	# another_request()
+
+def test13():
+	# 13 petición POST con un body grande no procesable de una vez
+	try:
+		file = open('html/test/un_archivo_cualquiera.txt', 'r')
+		body_content = file.read()
+		file.close()
+	except FileNotFoundError:
+		return
+	url = url_base + "/lorem_ipsum.txt"
+	headers['Host'] = 'localhost:8081'
+	headers['Content-Type'] = 'text/plain'
+	headers['Content-Length'] = str(len(body_content))
+	response = requests.post(url, headers=headers, data=body_content)
+	print("Longitud del body: " + str(len(body_content)))
+	try:
+		lorem = open("html/files/lorem_ipsum.txt", 'r')
+		lorem_content = lorem.read()
+		lorem.close()
+	except FileNotFoundError:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	file = open("serverHTML/postResponse.html", 'r')
+	content = file.read()
+	file.close()
+	if response.text == content and lorem_content == body_content:
+		print(Fore.GREEN + f"TEST {test} CORRECTO")
+	else:
+		print(Fore.RED + f"TEST {test} INCORRECTO")
+	try:
+		os.remove("html/files/lorem_ipsum.txt")
+	except FileNotFoundError:
+		print("No hay archivo")
+	print(Style.RESET_ALL)
+	print("\n" + "-"*50 + "\n")
+	# another_request()
+
+def test14():
+	# 14 request con chunks
+	pass
+
+def test15():
+	# 15 delete
+	pass
+
+def test16():
+	# 16 cgi basico
+	pass
+
+def test17():
+	# 17 cgi con viariables de entorno
+	pass
+
+def test18():
+	# 18 cgi duradero pero sin timeout
+	pass
+
+def test19():
+	# 19 cgi que llega a timeout
+	pass
+
+def test20():
+	# 20 cgi que da error
+	pass
+
+def test21():
+	# 21 cgi con post
+	pass
 
 
-another_request()
 
-# 9 servername incorrecto en header
-test += 1
-url = url_base + "/hola"
-headers['Host'] = 'localhost:8081'
-response = requests.get(url, headers=headers)
-print("Texto de la respuesta:\n", response.text)
-print("Código de respuesta:\n", response.status_code)
-print("Cabeceras de la respuesta:\n", response.headers)
-file = open("/test/redirect/prueba.html", 'r')
-content = file.read()
-file.close()
-if response.text == content: # añadir comprobación de response.status_code
-	print(Fore.GREEN + f"TEST {test} CORRECTO")
-else:
-	print(Fore.RED + f"TEST {test} INCORRECTO")
-print(Style.RESET_ALL)
+if __name__ == '__main__':
+	test1()
+	test2()
+	test3()
+	test4()
+	test5()
+	test6()
+	test7()
+	test8()
+	test9()
+	test10()
+	test11()
+	#test12() # el test 12 es llamado dentro del test 11
+	test13()
+	test14()
+	test15()
+	test16()
+	test17()
+	test18()
+	test19()
+	test20()
+	test21()
 
